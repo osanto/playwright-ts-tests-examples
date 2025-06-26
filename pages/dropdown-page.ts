50460a51f436a540da9de873cbe4c67cac631ce0
Olha Santo <olha.santo@gmail.com>
2025-06-26 18:27:19 -0500

    refactor: update dropdown page object and related tests for clarity and consistency

--- pages/dropdown-page.ts
+++ pages/dropdown-page.ts
@@ -3,19 +3,25 @@ import { BasePage } from './base-page';
 
 export class DropdownPage extends BasePage {
     readonly pageHeader: Locator;
-    readonly dropdown: Locator;
+    readonly dropdownElement: Locator;
+    readonly dropdownOptions: Locator;
 
     constructor(page: Page) {
         super(page);
         this.pageHeader = this.page.locator('h3');
-        this.dropdown = this.page.locator('#dropdown');
+        this.dropdownElement = this.page.locator('#dropdown');
+        this.dropdownOptions = this.dropdownElement.locator('option');
     }
 
     async getPageHeader() {
         return await this.pageHeader.innerText();
     }
 
-    async getDropdownValue() {
-        return await this.dropdown.inputValue();
+    async selectOptionByValue(value: string) {
+        await this.dropdownElement.selectOption(value);
+    }
+
+    getSelectedOption() {
+        return this.dropdownElement.locator('option:checked');
     }
 }
--- tests/dropdown.spec.ts
+++ tests/dropdown.spec.ts
@@ -8,9 +8,29 @@ test.describe('Dropdown page', () => {
     
     test('Verify page elements', async ({ dropdownPage }) => {
         const headerText = await dropdownPage.getPageHeader();
-        const dropdownElement = dropdownPage.dropdown;
+        const dropdownElement = dropdownPage.dropdownElement;
+        const dropdownOptions = dropdownPage.dropdownOptions;
 
         expect(headerText).toBe('Dropdown List');
-        await expect(dropdownElement).toBeVisible();
+        await expect.soft(dropdownElement).toBeVisible();
+        await expect.soft(dropdownOptions).toHaveCount(3);
+        await expect.soft(dropdownOptions.first()).toHaveText('Please select an option');
+    });
+
+    test('Verify dropdown options in DOM', async ({ dropdownPage }) => {
+        const dropdownOptions = dropdownPage.dropdownOptions;
+        await expect(dropdownOptions).toHaveCount(3);
+
+        const options = await dropdownOptions.allTextContents();
+        expect(options).toEqual(['Please select an option', 'Option 1', 'Option 2']);
+    });
+
+    test('Verify dropdown selection', async ({ dropdownPage }) => {
+        await dropdownPage.selectOptionByValue('1');
+        await expect(dropdownPage.dropdownElement).toHaveValue('1');
+
+        const selectedOption = dropdownPage.getSelectedOption();
+        await expect(selectedOption).toHaveText('Option 1');
+  
     });
 });
