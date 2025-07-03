8a923d9311031d026b74eb3ab3ca96ad50fd7640
Olha Santo <olha.santo@gmail.com>
2025-07-03 17:58:08 -0500

    feat: add two tests to key presses spec

--- tests/key-presses.spec.ts
+++ tests/key-presses.spec.ts
@@ -13,4 +13,23 @@ test.describe('Key Presses page', () => {
         await expect(keyPressesPage.description).toHaveText('Key presses are often used to interact with a website (e.g., tab order, enter, escape, etc.). Press a key and see what you inputted.');
         await expect(keyPressesPage.inputArea).toBeVisible();
     });
+
+    test('Verify key presses', async ({ keyPressesPage }) => {
+        const keys = ['H', '1', 'Shift', 'Space', 'Tab', 'Escape'];
+
+        for (const key of keys) {
+            await keyPressesPage.inputArea.press(key);
+            
+            await expect(keyPressesPage.enteredKey).toHaveText(`You entered: ${key.toUpperCase()}`);
+        }
+    });
+
+    test('Verify key presses with enter clears input', async ({ keyPressesPage }) => {
+        const key = 'Enter';
+
+        await keyPressesPage.inputArea.press(key);
+
+        await expect(keyPressesPage.inputArea).toHaveValue('');
+        await expect(keyPressesPage.enteredKey).not.toBeVisible();
+    });
 });
